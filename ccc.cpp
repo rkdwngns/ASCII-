4c0688f5210bbc0afd35fa3c57895e37b25b5479
rkdwngns <93520535+rkdwngns@users.noreply.github.com>
2021-12-16 11:33:54 +0900

    Add files via upload

---
+++ ccc.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+main () {
+    int x,y; /*루프 카운터*/    
+    char c; /*캐릭터 번호*/
+
+    for (x = 2; x<8; x++)
+        printf("16: 10:c | ");
+    printf("￦n");
+    for( x=2; x<8; x++ )
+        printf("---------+-");
+    printf("￦n");
+
+    for( y = 0; y<16; y++)
+	{
+        for( x = 2; x<8; x++) 
+		{
+            c = x * 16 +y;
+            printf("%2X:%3d:%c |", c,c,c);
+        }
+        printf("￦n");
+    }
+}
